4d58370b7fc23ee626f501605f333503d4c1d3f8
Domanat <maxim.tomilko@yandex.ru>
2020-03-18 16:27:09 +0300

    Core structure

---
+++ 02. Lesson/isomorphic.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func isomorphic(s string, t string) bool {
+
+	strMap := make(map[int]int)
+
+	return true
+}
+
+func main() {
+	str1 := "egg"
+	str2 := "adl"
+	if isomorphic(str1, str2) {
+		fmt.Println("Isomorphic")
+	} else {
+		fmt.Println("Not isomorphic")
+	}
+}
