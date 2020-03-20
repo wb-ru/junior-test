fafa9fb09cfe8d9fe14dc1a6b70f245e059de14a
DarkSidko <57124194+DarkSidko@users.noreply.github.com>
2020-03-21 00:17:01 +0500

    update for 3 Lesson

--- 03. Lesson/thirdres/thirdres.go
+++ 03. Lesson/thirdres/thirdres.go
@@ -5,9 +5,10 @@ import (
 	"time"
 )
 
-func workerPool(workers int) {
+func workerPool(workers int) int {
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
+	var counter int = 0
 	workerIn := make(chan int, workers)
 	workerOut := make(chan bool, workers)
 	for i := 0; i < workers; i++ {
@@ -16,6 +17,12 @@ func workerPool(workers int) {
 	for i := 0; i < workers; i++ {
 		workerIn <- r1.Intn(3) + 1
 	}
+	for i := 0; i < workers; i++ {
+		if <-workerOut == true {
+			counter = counter + 1
+		}
+	}
+	return counter
 }
 
 func startWorker(workerOut chan bool, workerIn chan int) {
