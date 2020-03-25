0e13cee0c4772b0871cb4ac6024cf997d47f5a2a
paavo <shibaev.pp@phystech.edu>
2020-03-25 12:52:00 -0400

    Made the correct concurrent and safe version of the worker pool.

--- 03.Lesson/worker_pool.go
+++ 03.Lesson/worker_pool.go
@@ -2,12 +2,16 @@ package worker_pool
 
 import (
 	"math/rand"
+	"sync"
 	"time"
 )
 
 type outputPayload struct{}
 
+var wg sync.WaitGroup
+
 func work(out chan *outputPayload) {
+	defer wg.Done()
 	d := rand.Int() % 3
 	time.Sleep(time.Second * time.Duration(d))
 	load := new(outputPayload)
@@ -15,13 +19,14 @@ func work(out chan *outputPayload) {
 }
 
 func manageWork(numWorkers int) {
+	wg.Add(numWorkers)
 	out := make(chan *outputPayload, numWorkers)
 	rand.Seed(1) // use some precise seed when debugging
 	// rand.Seed(time.Now().Unix())
 	for i := 0; i < numWorkers; i++ {
-		work(out)
+		go work(out)
 	}
-
+	wg.Wait()
 	for i := 0; i < numWorkers; i++ {
 		_ = <-out
 		// log.Printf("Received payload #%d %v", i, &rcv)
