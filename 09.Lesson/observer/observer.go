42b54a3c7786fd0ff0b690639cc0bb7f24bc374d
paavo <shibaev.pp@phystech.edu>
2020-03-28 15:56:08 -0400

    Lesson 09 Draft implementation of the observer pattern

---
+++ 09.Lesson/observer/observer.go
@@ -0,0 +1,49 @@
+package observer
+
+type Observer interface {
+	Handle() string
+}
+
+type Observed interface {
+	AddObserver(obs Observer)
+	RemoveObserver(obs Observer)
+	Notify() string
+}
+
+var Payload = []string{"1", "2"}
+
+type Obs1 struct {
+}
+
+func (p *Obs1) Handle() string {
+	return Payload[0]
+}
+
+type Obs2 struct{}
+
+func (p *Obs2) Handle() string {
+	return Payload[1]
+}
+
+type SomeObserved struct {
+	storage map[Observer]bool
+}
+
+func (p *SomeObserved) AddObserver(obs Observer) {
+	if p.storage == nil {
+		p.storage = make(map[Observer]bool)
+	}
+	p.storage[obs] = true
+}
+
+func (p *SomeObserved) RemoveObserver(obs Observer) {
+	delete(p.storage, obs)
+}
+
+func (p *SomeObserved) Notify() string {
+	res := ""
+	for key, _ := range p.storage {
+		res += key.Handle()
+	}
+	return res
+}
