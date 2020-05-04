51e2eb80b81426357155169fc78788cd09513962
DarkSidko <57124194+DarkSidko@users.noreply.github.com>
2020-05-04 11:36:18 +0500

    chain pattern

--- 07. Lesson/chainOfResponsibility/chain_test.go
+++ 07. Lesson/chainOfResponsibility/chain_test.go
@@ -1,1 +1,28 @@
-package chainOfResponsibility
+package chainofresponsibility
+
+import (
+	"testing"
+)
+
+func TestChainOfResponsibility(t *testing.T) {
+	hCritical := new(HandlerCritical)
+	hShops := new(HandlerShops)
+	hWork := new(HandlerWork)
+	hWork.next = hShops
+	hShops.next = hCritical
+	var tests = []struct {
+		message string
+		target  string
+	}{
+		{"Monitoring", "Message sent to critical folder."},
+		{"Shop", "Message sent to shop folder."},
+		{"Job", "Message sent to job folder."},
+		{"Spam", ""},
+	}
+	for _, test := range tests {
+		result := hWork.SendRequest(test.message)
+		if result != test.target {
+			t.Errorf("Expect result to be:%v, but got:%v", test.target, result)
+		}
+	}
+}
